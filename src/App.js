77ba215b15a51aa376637485101c973b29e92b9b
David Boles <me@davidbol.es>
2018-07-26 12:58:51 -0700

    Old notebook work and starting react design.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
 import Importer from './import/Importer';
+import CssBaseline from '@material-ui/core/CssBaseline'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Fade from '@material-ui/core/Fade';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#424242',
+    },
+    secondary: {
+      main: '#b0bec5',
+    },
+  },
+});
+
+const fadeTimeout = 100;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: null,
+      next: null,
+      isTransitioning: false
+    }
+  }
+
   render() {
     return (
-      <Importer/>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar>
+          <Toolbar variant="dense">
+            <Typography variant="title" color="inherit" style={{flexGrow: 1}}>
+              Papier Libéré
+            </Typography>
+            <Button variant='outlined' style={{color: 'white', borderColor: 'white'}}>
+              log in
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Fade in={!!this.state.next} timeout={fadeTimeout}>
+          <div style={{marginTop: 48}}>
+            {this.state.current}
+          </div>
+        </Fade>
+      </MuiThemeProvider>
     );
+  }
+
+  setView(component) {
+    this.setState({
+      next: component,
+      isTransitioning: true
+    }).then(() => {
+      if(!this.state.isTransitioning) {
+        setTimeout(this.setState({
+          current: this.state.next,
+          next: null,
+          isTransitioning: false
+        }), fadeTimeout);
+      }
+    });
   }
 }
 
--- templates/letter_notebook/generate.js
+++ templates/letter_notebook/generate.js
@@ -9,22 +9,25 @@ PDFDocument.prototype.addSVG = function(svg, x, y, options) {
 const PPI = 72;
 
 //CONFIG
-const pageWidth = 612;
-const pageHeight = 792;
-const pageStart = 99;//What page index to start on, should be odd.
-const pageCount = 2;//Should be even (actual sheets of paper is half this).
-const newPageOpts = {margin: 0}
+const bleedMargin = 0.25*PPI
+const doubleBleedMargin = 2*bleedMargin;
+const pageWidth = 612 + doubleBleedMargin;
+const pageHeight = 792 + doubleBleedMargin;
+const pageStart = 1;//What page index to start on, should be odd.
+const pageCount = 100;//Should be even (actual sheets of paper is half this).
+const newPageOpts = {size: [pageWidth, pageHeight],margin: 0}
+const insideCoverColor = '#404040';
 const pageNumColor = '#c0c0c0';
-const coverTemplate = fs.readFileSync('./assets/cover.svg').toString();
-const leftBindingTemplate = fs.readFileSync('./assets/page-dot_grid-left_binding.svg').toString();
-const rightBindingTemplate = fs.readFileSync('./assets/page-dot_grid-right_binding.svg').toString();
+const coverTemplate = fs.readFileSync('./assets/cover-with_bleed.svg').toString();
+const leftBindingTemplate = fs.readFileSync('./assets/page-dot_grid-left_binding-with_bleed.svg').toString();
+const rightBindingTemplate = fs.readFileSync('./assets/page-dot_grid-right_binding-with_bleed.svg').toString();
 
 //Generate
 const notebook = new PDFDocument({autoFirstPage: false});
-notebook.pipe(fs.createWriteStream('./notebook.pdf'));
+notebook.pipe(fs.createWriteStream('./notebook-with_bleed-fixed.pdf'));
 
 frontCover();
-notebook.fontSize(PPI/3);
+notebook.fontSize(PPI/3)
 for(var i = pageStart; i < pageStart+pageCount; i++) {
   contentPage(i);
 }
@@ -37,9 +40,9 @@ function contentPage(index) {
     width: pageWidth,
     height: pageHeight
   });
-  notebook.image(qr.imageSync(`1 LTR ${index-1}`, { margin: 0, size: 20 }), (bindingLeft ? 7.375 : 7)*PPI, 9.875*PPI, {width: 0.75*PPI, height: 0.75*PPI});
-  notebook.rect((bindingLeft ? 0.5 : 0.125)*PPI, 10.475*PPI, 16+(Math.ceil((notebook.widthOfString(index.toString()))/14.1732288)*14.1732288), 0.4*PPI).fill('white');
-  notebook.fillColor(pageNumColor).text(index, (bindingLeft ? 0.5 : 0.125)*PPI + (0.18*PPI), pageHeight - (PPI/3) - (0.18*PPI));
+  notebook.image(qr.imageSync(`1 LTR ${index}`, { margin: 0, size: 20 }), bleedMargin + ((bindingLeft ? 7.375 : 7)*PPI), (9.875*PPI)+bleedMargin, {width: 0.75*PPI, height: 0.75*PPI});
+  notebook.rect(bleedMargin+((bindingLeft ? 0.5 : 0.125)*PPI)+6, (10.475*PPI)+bleedMargin, 10+(Math.ceil((notebook.widthOfString(index.toString()))/14.1732288)*14.1732288), 24).fill('white');
+  notebook.fillColor(pageNumColor).text(index, (bindingLeft ? 0.5 : 0.125)*PPI + (0.18*PPI) + bleedMargin, pageHeight - (PPI/3) - (0.18*PPI) - bleedMargin);
 }
 
 function frontCover() {
@@ -49,17 +52,22 @@ function frontCover() {
     height: pageHeight
   });
   notebook.addPage(newPageOpts);
-  notebook.text('Inside front');
+  notebook.fontSize(PPI/5).fillColor(insideCoverColor).text('David Boles\nme@davidbol.es\n(415) 825-2464\n\n\n\n\n\n\nstart date: ______\nend date: ______', 0 + bleedMargin, (2.75*PPI)+bleedMargin, {
+    width: 8.125*PPI,
+    align: 'center',
+    lineGap: 0.375*PPI
+  });
 }
 
 function backCover() {
   notebook.addPage(newPageOpts);
-  notebook.text('Inside back');
   notebook.addPage(newPageOpts);
   notebook.addSVG(coverTemplate, 0, 0, {
     width: pageWidth,
     height: pageHeight
-  }); 
+  });
+
+  notebook.image(qr.imageSync('https://github.com/david476/papier_libere', { margin: 0, size: 20 }), (7*PPI)+bleedMargin, (9.875*PPI)+bleedMargin, {width: 0.75*PPI, height: 0.75*PPI});
 }
 
 //FINALIZE + END STREAM
