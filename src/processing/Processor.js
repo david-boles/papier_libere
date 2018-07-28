225e77ad93f37027f9c45150344945c2d9513baf
David Boles <me@davidbol.es>
2018-07-28 13:53:02 -0700

    Chrome mobile bug fix test

--- src/processing/Processor.js
+++ src/processing/Processor.js
@@ -8,54 +8,52 @@ class Processor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      current: <Importer/>,
-      next: null,
-      currentSlideIn: true,
-      nextSlideIn: false
+      view: <Importer/>,
+      direction: 'right',
+      in: true
     }
   }
 
-  render() {//<div style={{position: 'absolute', top: 48, width: '100vw', height: 'calc(100vh - 48px)'}}>
+  render() {
     return (
       <React.Fragment>
-        <Slide in={this.state.currentSlideIn} timeout={slideTimeout} direction='right' appear={false}>
-          <div style={{position: 'absolute', top: 48, width: this.state.nextSlideIn ? 0 : '100vw', height: 'calc(100vh - 48px)', overflowY: 'auto'}}>
-            {this.state.current}
-          </div>
-        </Slide>
-        <Slide in={this.state.nextSlideIn} timeout={slideTimeout} direction={'left'}>
-          <div style={{position: 'absolute', top: 48, width: this.state.nextSlideIn ? '100vw' : 0, height: 'calc(100vh - 48px)', overflowY: 'auto'}}>
-            {this.state.next}
-          </div>
-        </Slide>
+        {/* It seems dumb to do it this way but I had to to trick react into making separate Slides so that the directions actually worked. */
+          this.state.direction === 'left'?
+            <Slide in={this.state.in} timeout={slideTimeout} direction='left' appear={true}>
+              {this.state.view}
+            </Slide>
+          :
+            null
+        }
+        {
+          this.state.direction === 'right'?
+            <Slide in={this.state.in} timeout={slideTimeout} direction='right' appear={false}>
+              {this.state.view}
+            </Slide>
+          :
+            null
+        }
       </React.Fragment>
     );
   }
 
   setView(component) {
-    const wasTransitioning = !!this.state.next;
     this.setState({
-      next: component,
-      currentSlideIn: wasTransitioning ? this.state.currentSlideIn : false
+      in: false
     }, ()=>{
-      if(!wasTransitioning) {
-        console.log('wasn\'t transitioning')
-        setTimeout(() => {
-          this.setState({
-            current: null,
-            currentSlideIn: true,
-            nextSlideIn: true
-          }, () => {
-            setTimeout(()=>{
-              this.setState({
-                current: this.state.next,
-                next: null,
-                nextSlideIn: false
-              });
-            }, slideTimeout);
-          });
-        }, slideTimeout);
-      }
+      setTimeout(() => {
+        this.setState({
+          view: component,
+          direction: 'left',
+          in: true
+        }, () => {
+          setTimeout(() => {
+            this.setState({
+              direction: 'right'
+            });
+          }, slideTimeout);
+        });
+      }, slideTimeout);
     });
   }
 
