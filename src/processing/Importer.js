2d0646704a7b81c5af550015a5634d7ab7d46419
David Boles <me@davidbol.es>
2018-07-30 00:44:39 -0700

    Cleaned up importer imports and bumped version

--- src/processing/Importer.js
+++ src/processing/Importer.js
@@ -1,20 +1,14 @@
-import Slide from '@material-ui/core/Slide';
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
-import NoteAdd from '@material-ui/icons/NoteAdd';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 import Tooltip from '@material-ui/core/Tooltip';
-import OldImporter from '../import/Importer';
 import Check from '@material-ui/icons/Check';
 import Warning from '@material-ui/icons/Warning';
+import React, { Component } from 'react';
 
 class Importer extends Component {
   constructor(props) {
