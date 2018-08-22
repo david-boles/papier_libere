791edbc5e8d9ad952311a5bb6631a8b367f73846
David Boles <me@davidbol.es>
2018-08-21 21:16:17 -0700

    New notebook template

---
+++ templates/notebook-uncropped/generate.js
@@ -0,0 +1,40 @@
+const PDFDocument = require('pdfkit');
+const SVGtoPDF = require('svg-to-pdfkit');
+const fs = require('fs');
+const qr = require('qr-image');
+PDFDocument.prototype.addSVG = function(svg, x, y, options) {
+  return SVGtoPDF(this, svg, x, y, options), this;
+};
+
+const PPI = 72;
+
+//CONFIG
+const pageStart = 1;//What page index to start on, should be odd.
+const pageCount = 100;//Should be even (actual sheets of paper is half this).
+const pageNumColor = '#c0c0c0';
+const leftBindingTemplate = fs.readFileSync('./assets/page-dotted-left_binding.svg').toString();
+const rightBindingTemplate = fs.readFileSync('./assets/page-dotted-right_binding.svg').toString();
+
+//GENERATE
+const pages = new PDFDocument({autoFirstPage: false});
+pages.pipe(fs.createWriteStream('./notebook_pages-uncropped.pdf'));
+pages.fontSize(PPI/3)
+for(var i = pageStart; i < pageStart+pageCount; i++) {
+  contentPage(i);
+}
+pages.end();
+
+function contentPage(index) {
+  const bindingLeft = !!(index % 2);
+  pages.addPage({size: [8.5*PPI, 11*PPI], margin: 0});
+
+  pages.addSVG(bindingLeft ? leftBindingTemplate : rightBindingTemplate, 0, 0, {
+    width: 8.5*PPI,
+    height: 11*PPI
+  });
+
+  pages.image(qr.imageSync(`2 ${index}`, { ec_level: 'H', margin: 0, size: 20 }),  ((bindingLeft ? 7.125 : 7)*PPI), 9.625*PPI, {width: 0.75*PPI, height: 0.75*PPI});
+
+  pages.rect(((bindingLeft ? 0.5 : 0.375)*PPI)+6, 10.275*PPI, 10+(Math.ceil((pages.widthOfString(index.toString()))/14.1732288)*14.1732288), 24).fill('white');
+  pages.fillColor(pageNumColor).text(index, (bindingLeft ? 0.5 : 0.375)*PPI - 3 + (0.18*PPI), (10.75*PPI) - (PPI/3) - (0.13*PPI));
+}
