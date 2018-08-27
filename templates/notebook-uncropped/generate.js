b9512dbee6a5b6ed0471d62abfc977f6a6254c32
David Boles <me@davidbol.es>
2018-08-27 00:36:34 -0700

    New pages in template

--- templates/notebook-uncropped/generate.js
+++ templates/notebook-uncropped/generate.js
@@ -9,23 +9,38 @@ PDFDocument.prototype.addSVG = function(svg, x, y, options) {
 const PPI = 72;
 
 //CONFIG
-const pageStart = 1;//What page index to start on, should be odd.
+const name = 'Papier Libéré';
+const contactInfo = 'www.papierlibere.com';
+const pageStart = 101;//What page index to start on, should be odd.
 const pageCount = 100;//Should be even (actual sheets of paper is half this).
-const pageNumColor = '#c0c0c0';
+const textColor = '#bfbfbf';
 const leftBindingTemplate = fs.readFileSync('./assets/page-dotted-left_binding.svg').toString();
 const rightBindingTemplate = fs.readFileSync('./assets/page-dotted-right_binding.svg').toString();
+const cardScanTemplate = fs.readFileSync('./assets/card_scan.svg').toString();
 
 //GENERATE
 const pages = new PDFDocument({autoFirstPage: false});
-pages.pipe(fs.createWriteStream('./notebook_pages-uncropped.pdf'));
-pages.fontSize(PPI/3)
+pages.pipe(fs.createWriteStream(`./notebook_pages-uncropped-${pageStart}-${pageStart+pageCount-1}.pdf`));
+pages.fontSize(PPI/3);
+firstPage();
 for(var i = pageStart; i < pageStart+pageCount; i++) {
   contentPage(i);
 }
+cardScanPage();
 pages.end();
 
+function firstPage() {
+  pages.addPage({size: [8.5*PPI, 11*PPI], margin: 0});
+  pages.fillColor(textColor).text(name, 0.375*PPI, (11 - 0.25 - 0.6666)*PPI);
+  pages.fillColor(textColor).text(contactInfo, 0.375*PPI, (11 - 0.25 - 0.3333)*PPI);
+  const pagesText = `${pageStart}-${pageStart+pageCount-1}`;
+  pages.fillColor(textColor).text(pagesText, 8.25*PPI - pages.widthOfString(pagesText), (11 - 0.25 - 0.3333)*PPI);
+  pages.image(qr.imageSync(`https://www.papierlibere.com/`, { ec_level: 'H', margin: 0, size: 40 }),  3.5625*PPI, 3.4375*PPI, {width: 1.5*PPI, height: 1.5*PPI});
+  pages.flushPages();
+}
+
 function contentPage(index) {
-  const bindingLeft = !!(index % 2);
+  const bindingLeft = !(index % 2);
   pages.addPage({size: [8.5*PPI, 11*PPI], margin: 0});
 
   pages.addSVG(bindingLeft ? leftBindingTemplate : rightBindingTemplate, 0, 0, {
@@ -36,5 +51,21 @@ function contentPage(index) {
   pages.image(qr.imageSync(`2 ${index}`, { ec_level: 'H', margin: 0, size: 20 }),  ((bindingLeft ? 7.125 : 7)*PPI), 9.625*PPI, {width: 0.75*PPI, height: 0.75*PPI});
 
   pages.rect(((bindingLeft ? 0.5 : 0.375)*PPI)+6, 10.275*PPI, 10+(Math.ceil((pages.widthOfString(index.toString()))/14.1732288)*14.1732288), 24).fill('white');
-  pages.fillColor(pageNumColor).text(index, (bindingLeft ? 0.5 : 0.375)*PPI - 3 + (0.18*PPI), (10.75*PPI) - (PPI/3) - (0.13*PPI));
+  pages.fillColor(textColor).text(index, (bindingLeft ? 0.5 : 0.375)*PPI - 3 + (0.18*PPI), (10.75*PPI) - (PPI/3) - (0.13*PPI));
+  pages.flushPages();
+}
+
+function cardScanPage() {
+  pages.addPage({size: [8.5*PPI, 11*PPI], margin: 0});
+  pages.addSVG(cardScanTemplate, 0, 0, {
+    width: 8.5*PPI,
+    height: 11*PPI
+  });
+  pages.fillColor(textColor).text('business', (0.625 + 0.031 + 0.25)*PPI, (3.875 - 0.031 - 0.25)*PPI);
+  pages.image(qr.imageSync(`3 USBNS P`, { ec_level: 'H', margin: 0, size: 20 }),  1.875*PPI, 4.375*PPI, {width: 0.75*PPI, height: 0.75*PPI});
+  pages.fillColor(textColor).text('standard', (3.875 + 0.031 + 0.25)*PPI, (3.875 - 0.031 - 0.25)*PPI);
+  pages.image(qr.imageSync(`3 STNDRD P`, { ec_level: 'H', margin: 0, size: 20 }),  5.25*PPI, 4.375*PPI, {width: 0.75*PPI, height: 0.75*PPI});
+  pages.fillColor(textColor).text('index', (1.125 + 0.031 + 0.25)*PPI, (9.625 - 0.031 - 0.25)*PPI);
+  pages.image(qr.imageSync(`3 INDEX L`, { ec_level: 'H', margin: 0, size: 20 }),  6.875*PPI, 9.625*PPI, {width: 0.75*PPI, height: 0.75*PPI});
+  pages.flushPages();
 }
