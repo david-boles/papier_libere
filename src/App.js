6f9b703c87e405ace5ca374eb14d4f37402f4ba9
David Boles <me@davidbol.es>
2018-12-01 02:04:32 -0500

    Bumped minor version.

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
             <Typography variant="title" color="inherit" style={{flexGrow: 1}}>
               Papier Libéré
               <Typography variant='subheading' style={{display: 'inline', color: 'inherit', marginLeft: 8}}>
-                v0.1.3
+                v0.1.4
               </Typography>
             </Typography>
             {
